1ba482914afdc9e67b0e17085450cb91b05f12b4
JonKe <jonkerridge@btinternet.com>
2023-03-23 12:51:03 +0000

    Using version 1.0.2 and tested on a network comprising a host node plus three application nodes

---
+++ src/main/groovy/invoke/NetHost.groovy
@@ -0,0 +1,17 @@
+package invoke
+
+import cluster_cli.run.HostRun
+import jcsp.userIO.Ask
+import mandelbrot.MandelbrotCollect
+import mandelbrot.MandelbrotData
+
+class NetHost {
+  static void main(String[] args) {
+    String structureFile
+    if (args.size() == 0)
+      structureFile = Ask.string("Full pathname of the structure file? : ")
+    else
+      structureFile = args[0]
+    new HostRun(structureFile, MandelbrotData, MandelbrotCollect).invoke()
+  }
+}
