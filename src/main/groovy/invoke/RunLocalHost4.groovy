33307f99eb46f7d6796c2e948e60384b5aab3224
JonKe <jonkerridge@btinternet.com>
2023-03-24 19:03:30 +0000

    Uses v1.0.4 and more specifications created prior to testing on a large network plus sequential versions

--- src/main/groovy/invoke/RunLocalHost4.groovy
+++ src/main/groovy/invoke/RunLocalHost4.groovy
@@ -6,7 +6,7 @@ import mandelbrot.MandelbrotData
 
 class RunLocalHost4 {
   static void main(String[] args) {
-    String structureFile = "D:\\IJGradle\\ClusterMandelbrot\\src\\main\\groovy\\mandelbrotDSLfiles\\mandelbrot1n4w"
+    String structureFile = "D:\\IJGradle\\ClusterMandelbrot\\src\\main\\groovy\\mandelbrotDSLfiles\\mandelbrot1n4w512"
     Class  emitClass = MandelbrotData
     Class collectClass = MandelbrotCollect
     new HostRun(structureFile, emitClass, collectClass, "Local").invoke()
